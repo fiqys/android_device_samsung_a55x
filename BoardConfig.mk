00aee4550641a687cd339da21ed4bb10bb8e3046
lolipuru <banstellazz@gmail.com>
2025-05-01 16:32:55 +0700

    a55x: Use prebuilt kernel for now
    Change-Id: I294494817186f2a75a37d7180e4d8888bc990a5b
    Signed-off-by: lolipuru <banstellazz@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,16 +13,34 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+DEVICE_PATH := device/samsung/a55x
+TARGET_KERNEL_DIR := $(DEVICE_PATH)-kernel
+
 ## Inherit from the common tree
 include device/samsung/a55x-common/BoardConfigCommon.mk
 
 ## Inherit from the proprietary configuration
 include vendor/samsung/a55x/BoardConfigVendor.mk
 
-DEVICE_PATH := device/samsung/a55x
+# Prebuilts
+BOARD_PREBUILT_DTBOIMAGE := $(TARGET_KERNEL_DIR)/dtbo.img
+BOARD_PREBUILT_DTBIMAGE_DIR := $(TARGET_KERNEL_DIR)/dtb
 
-## Kernel
-TARGET_KERNEL_CONFIG := essi_defconfig
+TARGET_NO_KERNEL_OVERRIDE := true
+TARGET_KERNEL_SOURCE := $(TARGET_KERNEL_DIR)/kernel-headers
+PRODUCT_COPY_FILES += \
+	$(TARGET_KERNEL_DIR)/kernel:kernel
+
+# Kernel modules
+BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(TARGET_KERNEL_DIR)/vendor_ramdisk/modules.load))
+BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(TARGET_KERNEL_DIR)/vendor_dlkm/modules.load))
+BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(TARGET_KERNEL_DIR)/system_dlkm/modules.load))
+SYSTEM_KERNEL_MODULES := $(BOARD_SYSTEM_KERNEL_MODULES_LOAD)
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/vendor_dlkm/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) \
+    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
+    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules)
 
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
