83912d6ff5b626a1e412011eec469281b201bd40
ExtremeXT <extremext@lineageos.org>
2026-02-10 16:21:42 +0200

    a55x: Add shim for libepicoperator
    Change-Id: Ic171be82ad2916cb175fd4ac028af014b732a39a
    Signed-off-by: ExtremeXT <extremext@lineageos.org>
    Signed-off-by: fiqys <elsifycritic@gmail.com>

---
+++ shims/libepicoperator/epicoperator.c
@@ -0,0 +1,9 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+void* createScenarioOperator(int operator_id __attribute__((unused)),
+                             int scenario_id __attribute__((unused))) {
+    return nullptr;
+}
