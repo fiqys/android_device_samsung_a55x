dc107a33131fe60933ac5e371a36a6bfd2aeca76
fiqxty <elsifycritic@gmail.com>
2026-04-07 19:33:15 +0300

    a55x: Drop prebuilt kernel

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -22,25 +22,5 @@ include device/samsung/a55x-common/BoardConfigCommon.mk
 ## Inherit from the proprietary configuration
 include vendor/samsung/a55x/BoardConfigVendor.mk
 
-# Prebuilts
-BOARD_PREBUILT_DTBOIMAGE := $(TARGET_KERNEL_DIR)/dtbo.img
-BOARD_PREBUILT_DTBIMAGE_DIR := $(TARGET_KERNEL_DIR)/dtb
-
-TARGET_NO_KERNEL_OVERRIDE := true
-TARGET_KERNEL_SOURCE := $(TARGET_KERNEL_DIR)/kernel-headers
-PRODUCT_COPY_FILES += \
-	$(TARGET_KERNEL_DIR)/kernel:kernel
-
-# Kernel modules
-BOARD_VENDOR_RAMDISK_KERNEL_MODULES_LOAD := $(strip $(shell cat $(TARGET_KERNEL_DIR)/vendor_ramdisk/modules.load))
-BOARD_VENDOR_KERNEL_MODULES_LOAD := $(strip $(shell cat $(TARGET_KERNEL_DIR)/vendor_dlkm/modules.load))
-BOARD_SYSTEM_KERNEL_MODULES_LOAD := $(strip $(shell cat $(TARGET_KERNEL_DIR)/system_dlkm/modules.load))
-SYSTEM_KERNEL_MODULES := $(BOARD_SYSTEM_KERNEL_MODULES_LOAD)
-
-PRODUCT_COPY_FILES += \
-    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/vendor_dlkm/,$(TARGET_COPY_OUT_VENDOR_DLKM)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/vendor_ramdisk/,$(TARGET_COPY_OUT_VENDOR_RAMDISK)/lib/modules) \
-    $(call find-copy-subdir-files,*,$(TARGET_KERNEL_DIR)/system_dlkm/,$(TARGET_COPY_OUT_SYSTEM_DLKM)/lib/modules)
-
 ## Properties
 TARGET_VENDOR_PROP += $(DEVICE_PATH)/vendor.prop
