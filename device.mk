202791d5ca5d0566bf3a90ac7a79f0911885fbe4
ExtremeXT <extremext@lineageos.org>
2026-08-24 16:17:42 +0300

    a55x: Build SGR Gralloc AIDL service
    Change-Id: I32d52f5ebe1c2dc5c6d7d40c13335f042dcbd78d
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -107,7 +107,9 @@ PRODUCT_PACKAGES += android.hardware.gatekeeper-service.teegris
 
 # Graphics
 PRODUCT_PACKAGES += \
-    android.hardware.composer.hwc3-service.slsi
+    android.hardware.composer.hwc3-service.slsi \
+    android.hardware.graphics.allocator-aidl-service-sgr \
+    android.hardware.graphics.mapper@4.0-impl-sgr
 
 # Health
 PRODUCT_PACKAGES += \
