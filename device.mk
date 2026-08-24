f4c3274a82cca0fa2f3e4760340d456155a91468
Stricted <info@stricted.net>
2026-08-24 15:07:36 +0300

    a55x: Exclude AudioFX
    [Linux4: Move to product makefile as required after CL 299445]
    
    Change-Id: Id55bfec7af630a768cd6e14022f988f8c5b2a2a1
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -70,6 +70,8 @@ PRODUCT_PACKAGES += \
     bluetooth_with_le_audio_policy_configuration_7_0.xml \
     usbv2_audio_policy_configuration.xml
 
+TARGET_EXCLUDES_AUDIOFX := true
+
 # Boot Control
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.exynos \
