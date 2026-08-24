c30cdbf85cf17fb2a5602fe44e3d4080de0e7b10
Secondarian <robertanitei@tutanota.com>
2026-08-24 19:37:32 +0300

    a55x: Setup RIL blobs
    Change-Id: I3b3608480947cf5f13ef7f312f435bbc8254c913
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -209,6 +209,8 @@ PRODUCT_PACKAGES += \
     android.hardware.sensor.proximity.prebuilt.xml \
     android.hardware.sensor.stepcounter.prebuilt.xml \
     android.hardware.sensor.stepdetector.prebuilt.xml \
+    android.hardware.telephony.gsm.prebuilt.xml \
+    android.hardware.telephony.satellite.prebuilt.xml \
     android.hardware.usb.accessory.prebuilt.xml \
     android.hardware.usb.host.prebuilt.xml \
     android.hardware.vulkan.compute-0.prebuilt.xml \
