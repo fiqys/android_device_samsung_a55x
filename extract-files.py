efa75cff6957c7e54bd3b18be9a924fad99fce44
Tim Zimmerman <tim@linux4.de>
2026-08-24 17:43:34 +0300

    a55x: NOP SecCameraToRil::enable/disable m_sendRequest()
    * Fixes switching to different camera IDs when using OSS camera
      provider.
    
    Co-authored-by: Josip K <kelecicj@gmail.com>
    Change-Id: I489b505404750c7abb31c4bb86839a61f1d0542b
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -33,7 +33,9 @@ blob_fixups: blob_fixups_user_type = {
     'vendor/etc/media_codecs_performance_c2.xml': blob_fixup()
         .regex_replace('.*sec\\.(.|\n)*D', '    </D'),
     'vendor/lib64/hw/camera.s5e8845.so': blob_fixup()
-       .add_needed('libui_shim.so'),
+        .sig_replace('e7 89 01 94', '1f 20 03 d5')  # NOP VendorCameraIPCtoRIL::enable m_sendRequest()
+        .sig_replace('92 89 01 94', '1f 20 03 d5') # NOP VendorCameraIPCtoRIL::disable m_sendRequest()
+        .add_needed('libui_shim.so'),
     'vendor/lib64/libskeymint_cli.so': blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
     (
