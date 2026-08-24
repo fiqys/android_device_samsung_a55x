3096085a9a1f30c977a97a2b33b8e7a03df54e82
fiqys <elsifycritic@gmail.com>
2026-08-24 14:48:23 +0300

    a55x: Setup TEEGRIS blobs
    * A556BXXSABYI1 will be the default, A556EXXSABYI4 will be the one that will be mounted
    
    Change-Id: I3a64751e961788947d988212bd21f21b54c8255d
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -10,6 +10,11 @@ from extract_utils.fixups_blob import (
     blob_fixups_user_type,
 )
 
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
+
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -25,6 +30,15 @@ namespace_imports = [
 
 blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    'libuuid': lib_fixup_vendor_suffix,
+}
 
 module = ExtractUtilsModule(
     'a55x',
