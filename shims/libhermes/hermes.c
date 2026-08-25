fa63134ccde6696b16dbccdb022188e04a51fcdd
fiqys <elsifycritic@gmail.com>
2026-08-25 16:32:43 +0300

    a55x: Shim libhermes
    Change-Id: I39e1db6cfc1f00d5f508d8bac1ac57ae3a76c8a5
    Signed-off-by: fiqys <elsifycritic@gmail.com>

---
+++ shims/libhermes/hermes.c
@@ -0,0 +1,12 @@
+/*
+ * SPDX-FileCopyrightText: The LineageOS Project
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+int hermes_power_on(void) {
+    return 0;
+}
+
+int hermes_power_off(void) {
+    return 0;
+}
