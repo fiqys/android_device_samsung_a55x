b68765ce336be65aa51a8bd77ebdd8d2cd5670bc
fiqys <elsifycritic@gmail.com>
2026-08-26 22:32:07 +0300

    a55x: Import mixer paths
    * Only difference from stock is it's reformated
    
    Change-Id: Ib50b570e17f501a8c6bd4c9256751d8647526ab4
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- device.mk
+++ device.mk
@@ -73,6 +73,7 @@ PRODUCT_PACKAGES += \
     audio.r_submix.default \
     audio.usbv2.default \
     bluetooth_with_le_audio_policy_configuration_7_0.xml \
+    mixer_paths.xml \
     usbv2_audio_policy_configuration.xml
 
 TARGET_EXCLUDES_AUDIOFX := true
