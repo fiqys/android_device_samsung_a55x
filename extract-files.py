b82dc61c466de3d5c937fce2fd853bcb62f9ce16
Secondarian <robertanitei@tutanota.com>
2026-09-13 17:54:45 +0300

    a55x: Kill SEC MPEG4 virtual decoder
    Change-Id: Ibad2b6a934811a0afcb8c1e1573fe3f23e0ec8f5
    Signed-off-by: fiqys <elsifycritic@gmail.com>

--- extract-files.py
+++ extract-files.py
@@ -30,6 +30,8 @@ namespace_imports = [
 ]
 
 blob_fixups: blob_fixups_user_type = {
+    'vendor/etc/media_codecs_performance_c2.xml': blob_fixup()
+        .regex_replace('.*sec\\.(.|\n)*D', '    </D'),
     'vendor/lib64/libskeymint_cli.so': blob_fixup()
         .replace_needed('libcrypto.so', 'libcrypto-v33.so'),
     (
